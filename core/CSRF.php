991e92ae572eaf47c05a2e5b40d6bf21cac3ae8d
Ahmed Abu Ngeara <aabungeara@gmail.com>
2026-06-01 18:07:21 +0300

    Implement CSRF token generation and validation

---
+++ core/CSRF.php
@@ -0,0 +1,27 @@
+<?php
+
+class CSRF
+{
+    public static function generateToken(): string
+    {
+        $token = bin2hex(random_bytes(32));
+
+        $_SESSION["csrf_token"] = $token;
+
+        return $token;
+    }
+
+    public static function validateToken(string $token): bool
+    {
+        if (
+            !isset($_SESSION["csrf_token"])
+        ) {
+            return false;
+        }
+
+        return hash_equals(
+            $_SESSION["csrf_token"],
+            $token
+        );
+    }
+}
