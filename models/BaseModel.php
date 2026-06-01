67973468d01349656e59416c0d36a1ea9dbfe5b7
Ahmed Abu Ngeara <aabungeara@gmail.com>
2026-06-01 20:45:04 +0300

    Implement BaseModel for centralized DB access

---
+++ models/BaseModel.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . "/../core/Database.php";
+
+abstract class BaseModel
+{
+    protected Database $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+
+    protected function execute(
+        string $sql,
+        string $types = "",
+        array $params = []
+    ) {
+        try {
+
+            return $this->db->query(
+                $sql,
+                $types,
+                $params
+            );
+
+        } catch (Throwable $e) {
+
+            throw new RuntimeException(
+                "Database operation failed"
+            );
+        }
+    }
+}
